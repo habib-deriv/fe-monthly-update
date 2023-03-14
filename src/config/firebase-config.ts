bfdfd82270d5f2a1b98a3eb96a48e4ea809aaace
Eduard Hrachou <eduardhrachou@Eduard-Hrachous-MacBook-Pro-Y24T3WTX5M.local>
2023-03-14 12:24:41 +0300

    project setting up

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -4,7 +4,6 @@ import { getFirestore } from '@firebase/firestore';
 const firebaseConfig = {
     apiKey           : import.meta.env.VITE_FIRE_BASE_KEY,
     authDomain       : import.meta.env.VITE_FIRE_BASE_AUTH_DOMAIN,
-    databaseURL      : import.meta.env.VITE_FIRE_BASE_DATABASE_URL,
     projectId        : import.meta.env.VITE_FIRE_BASE_PROJECT_ID,
     storageBucket    : import.meta.env.VITE_FIRE_BASE_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIRE_BASE_MESSAGING_SENDER_ID,
